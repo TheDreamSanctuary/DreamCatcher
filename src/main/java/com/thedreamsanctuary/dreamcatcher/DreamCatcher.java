544c2e042209b635297a675950f8959ea9535209
Joe Kent <thedeadlybutter@gmail.com>
2014-03-31 16:01:51 -0400

    Initial commit

---
+++ src/main/java/com/thedreamsanctuary/dreamcatcher/DreamCatcher.java
@@ -0,0 +1,18 @@
+package com.thedreamsanctuary.dreamcatcher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by josephkent on 3/31/14.
+ */
+public class DreamCatcher extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
